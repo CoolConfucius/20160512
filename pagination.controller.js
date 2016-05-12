efdce88e21ce479db9d3765a27be68398b288530
Benjamin Huang <coolconfucius@yahoo.com>
2016-05-12 10:51:57 -0700

    1051

---
+++ pagination.controller.js
@@ -0,0 +1,11 @@
+jQuery.sap.require("jquery.sap.history");
+jQuery.sap.require("sap.m.InstanceManager");
+
+//(function() {
+  //'use strict';
+  sap.ui.controller("pagination.pagination", {
+    onInit : function() {
+
+    }
+  })
+//})
